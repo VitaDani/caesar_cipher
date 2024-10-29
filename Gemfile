50eecc49ef376b69d5d687701cb6328d47c37c84
Daniel Vitacolonna <44033488+VitaDani@users.noreply.github.com>
2024-10-29 15:03:50 -0600

    Learning more about ruby.
    Learning about Rubocop and linting/formatting.

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+# gem "rails"
+
+gem 'rubocop', '~> 1.67'
+gem 'rubocop-performance', require: false
--- cipher.rb
+++ cipher.rb
@@ -1,7 +1,8 @@
+# Function to convert a string to a new string by however many characters
+
 def caesar_cipher(string_to_modify, num_to_shift_string_chars)
-  numerical = string_to_modify.split('').map {|character| character.ord + num_to_shift_string_chars}
-  revised = numerical.map {|number| number.chr}.join
-  return revised
+  numerical = string_to_modify.chars.map { |character| character.ord + num_to_shift_string_chars }
+  return numerical.map(&:chr).join
 end
 
-caesar_cipher("abcdefg", 5)
+puts caesar_cipher('abcdefg', 5)
